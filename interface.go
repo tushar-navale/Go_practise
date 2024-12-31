9429edda17895e72dd8df7fe5a486bab4e44cd57
tushar-navale <tushar29ns@gmail.com>
2024-12-31 13:13:05 +0530

    interface practise

--- interface.go
+++ interface.go
@@ -15,6 +15,10 @@ type Circle struct {
 	radius float64
 }
 
+type Square struct {
+	side float64
+}
+
 // Implement the Shape interface for Circle
 func (c Circle) area() float64 {
 	return 3.14 * c.radius * c.radius
@@ -38,18 +42,31 @@ func (r Rectangle) perimeter() float64 {
 	return 2 * (r.width + r.height)
 }
 
+func (s Square) area() float64 {
+	return s.side * s.side
+}
+
+func (s Square) perimeter() float64 {
+	return 4 * s.side
+}
+
 func main() {
 	// Create instances of Circle and Rectangle
-	circle := Circle{radius: 5}
-	rectangle := Rectangle{width: 10, height: 5}
+	circle := Circle{radius: 345.33}
+	rectangle := Rectangle{width: 10.37, height: 5.343}
+	square := Square{side: 12.23}
 
 	// Create a slice of Shape interface
 	// as both circle and rectangle implement have the area and perimeter methods it is a part of the shape interface
-	shapes := []Shape{circle, rectangle}
-
+	shapes := []Shape{circle, rectangle, square}
+	// this shape will automatically call the area and perimeter methods of the circle and rectangle
+	// as the circle, rectangle and shape are part of the shape interface
 	// Iterate over the slice and call the methods
 	// here _ is used as we dont need the index, the 1st value is the index and the 2nd value is the shape
-	for _, shape := range shapes {
+	for index, shape := range shapes {
+		if index == 0 {fmt.Println("Circle")
+		} else if index == 1 {fmt.Println("Rectangle")
+		} else {fmt.Println("Square")}
 		fmt.Println("Area:", shape.area())
 		fmt.Println("Perimeter:", shape.perimeter())
 	}

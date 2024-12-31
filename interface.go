14a5774da0b4a3734c88baf12d4bb922233d5dc2
tushar-navale <tushar29ns@gmail.com>
2024-12-31 12:55:03 +0530

    basics array defer recover and interface

--- interface.go
+++ interface.go
@@ -44,9 +44,11 @@ func main() {
 	rectangle := Rectangle{width: 10, height: 5}
 
 	// Create a slice of Shape interface
+	// as both circle and rectangle implement have the area and perimeter methods it is a part of the shape interface
 	shapes := []Shape{circle, rectangle}
 
 	// Iterate over the slice and call the methods
+	// here _ is used as we dont need the index, the 1st value is the index and the 2nd value is the shape
 	for _, shape := range shapes {
 		fmt.Println("Area:", shape.area())
 		fmt.Println("Perimeter:", shape.perimeter())
